edf37bbc8859b6e1081d47be37cfcad3871976ed
Cedric Rollo <cedric.rollo@gmail.com>
2024-04-01 19:17:39 +0200

    🎉 🚀

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { authMiddleware } from '@clerk/nextjs'
+
+export default authMiddleware({
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing',
+    '/assets/images/dotted-pattern.png',
+    '/assets/images/logo.svg',
+    '/favicon.ico',
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing',
+  ],
+})
+
+export const config = {
+  matcher: ['/((?!.+.[w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+}
